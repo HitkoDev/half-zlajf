4bde72cd9b32e879e2fd0ccc6d43254ce7fb3f44
Hitko Development <development@hitko.si>
2014-12-18 22:17:42 +0100

    Library

--- HalfZlajf/src/game/Main.java
+++ HalfZlajf/src/game/Main.java
@@ -3,48 +3,46 @@ package game;
 public class Main  {
 
     public static void main(String[] args) {
-        String n = "Natives";
+        setNatives();
+        
+        new HelloWorld().run();
+    }
+    
+    // Funkcija, ki pripravi native knjižnjice za interakcijo z OpenGLjem
+    private static void setNatives(){
+        // Ugotovi, katere native datoteke potrebuje
+        String natives = "";
         String osName = System.getProperty("os.name");
         if(osName.startsWith("Windows"))
-            n += "/windows";
+            natives += "windows";
         else if(osName.startsWith("Linux") || osName.startsWith("FreeBSD") || osName.startsWith("SunOS") || osName.startsWith("Unix"))
-            n += "/linux";
+            natives += "linux";
         else if(osName.startsWith("Mac OS X") || osName.startsWith("Darwin"))
-            n += "/macosx";
+            natives += "macosx";
         else
             throw new LinkageError("Unknown platform: " + osName);
-        
-        n += "/" + (System.getProperty("os.arch").contains("64") ? "x64" : "x86");
-        
+        natives += "/" + (System.getProperty("os.arch").contains("64") ? "x64" : "x86");
         try {
             java.util.jar.JarFile jarfile = new java.util.jar.JarFile(new java.io.File("lib/assets.jar")); //jar file path(here sqljdbc4.jar)
             java.util.Enumeration<java.util.jar.JarEntry> enu= jarfile.entries();
+            String destdir = "natives";     // Target folder
             while(enu.hasMoreElements()) {
-                String destdir = "natives";   
                 java.util.jar.JarEntry je = enu.nextElement();
-
-                if(!je.getName().startsWith(n)) continue;
-                String name = je.getName().substring(8);
-                if(name.isEmpty()) continue;
-
+                String name = je.getName().length() > 8 ? je.getName().substring(8) : "";
+                if(je.isDirectory() || !name.startsWith(natives)) continue;     // Preskoči nepotrebe datoteke
                 java.io.File fl = new java.io.File(destdir, name);
                 if(!fl.exists()) {
                     fl.getParentFile().mkdirs();
-                    fl = new java.io.File(destdir, name);
+                    java.io.InputStream is = jarfile.getInputStream(je);
+                    java.io.FileOutputStream fo = new java.io.FileOutputStream(fl);
+                    while(is.available()>0) fo.write(is.read());
+                    fo.close();
+                    is.close();
                 }
-                if(je.isDirectory()) continue;
-                java.io.InputStream is = jarfile.getInputStream(je);
-                java.io.FileOutputStream fo = new java.io.FileOutputStream(fl);
-                while(is.available()>0) fo.write(is.read());
-                fo.close();
-                is.close();
             }
-
-            System.setProperty("java.library.path", "natives/");
+            System.setProperty("java.library.path", "natives/");            // Uporabi datoteke iz jara
         } catch (Exception ex) {
-            System.setProperty("java.library.path", "assets/Natives/");
+            System.setProperty("java.library.path", "assets/Natives/");     // Fallback za poganjanje iz netbeansa
         }
-        
-        new HelloWorld().run();
     }
 }
